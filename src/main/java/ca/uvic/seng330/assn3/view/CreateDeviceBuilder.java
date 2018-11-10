3a25244b477c1fec6a58538cda51fa3055e44f57
Pando <Pando@134.87.173.186>
2018-11-09 18:28:59 -0800

    planning for second half of createDevice View.

--- src/main/java/ca/uvic/seng330/assn3/view/CreateDeviceBuilder.java
+++ src/main/java/ca/uvic/seng330/assn3/view/CreateDeviceBuilder.java
@@ -7,6 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -19,9 +20,10 @@ public class CreateDeviceBuilder extends SceneBuilder {
 
   @Override
   protected Node buildSpecifics() {
-    HBox hbox = new HBox(10);
-
-    hbox.getChildren().add(new Label("Device Type"));
+    VBox vbox = new VBox(10);
+	  
+	  HBox topHalf = new HBox(10);
+    topHalf.getChildren().add(new Label("Device Type"));
 
     ScrollPane layout = new ScrollPane();
     layout.setFitToHeight(true);
@@ -31,15 +33,37 @@ public class CreateDeviceBuilder extends SceneBuilder {
 
     // TODO: review import DeviceType from controller
     ArrayList<DeviceType> deviceTypes = getController().getDeviceTypes();
-    VBox vbox = new VBox();
+    VBox typesForScroll = new VBox();
     RadioButton button;
     for (int i = 0; i < deviceTypes.size(); i++) {
       button = new RadioButton(deviceTypes.get(i).toString());
       button.setUserData(deviceTypes.get(i));
-      vbox.getChildren().add(button);
+      typesForScroll.getChildren().add(button);
     }
-    hbox.getChildren().add(vbox);
+    topHalf.getChildren().add(typesForScroll);
+    vbox.getChildren().add(topHalf);
 
-    return hbox;
+    vbox.getChildren().add(new Label("=============================="));
+    
+    HBox lowerHalf = new HBox(10);
+    
+    VBox lowerLeft = new VBox(10);
+    lowerLeft.getChildren().add(new Label("Device Status"));
+    TextField customLabel = new TextField();
+    customLabel.setPromptText("Label");
+    lowerLeft.getChildren().add(customLabel);
+    lowerHalf.getChildren().add(lowerLeft);
+    
+    HBox lowerMiddle = new HBox(10);
+    //TODO: add radio buttons here
+    HBox lowerRight = new HBox(10);
+    //TODO: add create device button here
+    
+    lowerHalf.getChildren().add(lowerMiddle);
+    lowerHalf.getChildren().add(lowerRight);
+    
+    vbox.getChildren().add(lowerHalf);
+    
+    return vbox;
   }
 }
