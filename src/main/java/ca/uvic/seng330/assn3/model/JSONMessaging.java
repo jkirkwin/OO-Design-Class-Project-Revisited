8a4ca06c79b0877775c71818262a257d2a655eca
Jamie Kirkwin <jkirkwin@gmail.com>
2018-11-11 20:05:23 -0800

    Changed message IDs to randomly chosen UUIDs for ease of re-creation

--- src/main/java/ca/uvic/seng330/assn3/model/JSONMessaging.java
+++ src/main/java/ca/uvic/seng330/assn3/model/JSONMessaging.java
@@ -1,16 +1,18 @@
 package ca.uvic.seng330.assn3.model;
 
 import ca.uvic.seng330.assn3.model.devices.Device;
+import ca.uvic.seng330.assn3.model.storage.Storage;
+
 import java.util.Date;
+import java.util.UUID;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class JSONMessaging {
 
-  static int messagesSent = 0;
   Device talker;
   String message = "test message";
-  private final int messageNumber = messagesSent + 1;
 
   public JSONMessaging(Device talker, String message) {
     this.talker = talker;
@@ -20,13 +22,13 @@ public class JSONMessaging {
   public JSONObject invoke() {
     JSONObject json = new JSONObject();
     try {
-      // TODO: increment msg_id
-      json.put("msg_id", messageNumber);
+      json.put("msg_id", Storage.getJsonUUID(UUID.randomUUID()));
       json.put("node_id", talker.getIdentifier());
       json.put("status", talker.getStatus());
       json.put("payload", message);
       json.put("created_at", new Date());
     } catch (JSONException e) {
+      // TODO Log failure
     }
     return json;
   }
@@ -34,4 +36,6 @@ public class JSONMessaging {
   public Device getTalker() {
     return this.talker;
   }
+  
+  
 }
