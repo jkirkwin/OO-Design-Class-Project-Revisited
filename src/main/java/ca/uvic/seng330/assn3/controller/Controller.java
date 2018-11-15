fe821fec79ed97652ff5d75788eb08b698560f9a
Jamie Kirkwin <jkirkwin@gmail.com>
2018-11-14 22:01:25 -0800

    Added LoginController

--- src/main/java/ca/uvic/seng330/assn3/controller/Controller.java
+++ src/main/java/ca/uvic/seng330/assn3/controller/Controller.java
@@ -34,7 +34,7 @@ public class Controller {
 
   protected final Hub hub;
   protected final Client client;
-  private UserAccount activeUser;
+  protected UserAccount activeUser;
   protected final Stack<ViewType> views;
   private final SceneBuilder loginBuilder;
 
@@ -113,7 +113,7 @@ public class Controller {
   /*
    * @pre view != null
    */
-  private SceneBuilder findBuilder(ViewType view) {
+  SceneBuilder findBuilder(ViewType view) {
     assert view != null;
     // TODO generate the appropriate SceneBuilder based on for the ViewType
     views.push(view);
@@ -162,23 +162,7 @@ public class Controller {
    * @pre password != null
    */
   public void handleLoginClick(String username, String password) {
-    assert username != null;
-    assert password != null;
-    if (hub.isUser(username)) {
-      System.out.println("Logged in"); // Testing
-      this.activeUser = hub.getUser(username, password);
-      if (this.activeUser.isAdmin()) {
-        client.setView(findBuilder(ViewType.HUB_ADMIN));
-      } else {
-        client.setView(findBuilder(ViewType.HUB_BASIC));
-      }
-    } else {
-      client.alertUser(
-          AlertType.INFORMATION,
-          "Failure",
-          "Account does not exist",
-          "Username \"" + username + "\" is not in use. Please try a different one.");
-    }
+    LoginController.handleLoginClick(this, hub, client, username, password);
   }
 
   /*
@@ -186,20 +170,7 @@ public class Controller {
    * @pre password != null
    */
   public void handleNewUser(String username, String password) {
-    assert username != null;
-    assert password != null;
-    if (acceptableInputs(username, password)) {
-      if (!hub.isUser(username)) {
-        hub.register(new UserAccount(this.hub, AccessLevel.BASIC, username, password));
-        client.alertUser(AlertType.INFORMATION, "Success", "Success", "User Account Created.");
-      } else {
-        client.alertUser(
-            AlertType.INFORMATION,
-            "Failure",
-            "Username Unavailable",
-            "Username \"" + username + "\" is already in use. Please try a different one.");
-      }
-    }
+    LoginController.handleNewUser(hub, client, username, password);
   }
 
   /*
@@ -207,45 +178,7 @@ public class Controller {
    * @pre password != null
    */
   public void handleNewAdmin(String username, String password) {
-    assert username != null;
-    assert password != null;
-    if (acceptableInputs(username, password)) {
-      if (!hub.isUser(username)) {
-        hub.register(new UserAccount(this.hub, AccessLevel.ADMIN, username, password));
-        client.alertUser(AlertType.INFORMATION, "Success", "Success", "Admin Account Created.");
-      } else {
-        client.alertUser(
-            AlertType.INFORMATION,
-            "Failure",
-            "Username Unavailable",
-            "Username \"" + username + "\" is already in use. Please try a different one.");
-      }
-    }
-  }
-
-  /*
-   * @pre username != null
-   * @pre password != null
-   */
-  private boolean acceptableInputs(String username, String password) {
-    assert username != null;
-    assert password != null;
-    if (username.toLowerCase().equals("username") || username.isEmpty()) {
-      client.alertUser(
-          AlertType.INFORMATION,
-          "Failure",
-          "Username Unaccepted",
-          "Username is not allowed to be \"username\" or left blank. Please try again");
-      return false;
-    } else if (password.toLowerCase() == "password" || password.isEmpty()) {
-      client.alertUser(
-          AlertType.INFORMATION,
-          "Failure",
-          "Password Unaccepted",
-          "Password is not allowed to be \"password\" or left blank. Please try again");
-      return false;
-    }
-    return true;
+    LoginController.handleNewAdmin(hub, client, username, password);
   }
 
   // ============================ Admin Console =========================//
---
+++ src/main/java/ca/uvic/seng330/assn3/controller/LoginController.java
@@ -0,0 +1,100 @@
+package ca.uvic.seng330.assn3.controller;
+
+import ca.uvic.seng330.assn3.model.AccessLevel;
+import ca.uvic.seng330.assn3.model.Hub;
+import ca.uvic.seng330.assn3.model.UserAccount;
+import ca.uvic.seng330.assn3.view.Client;
+import javafx.scene.control.Alert.AlertType;
+
+public class LoginController {
+  /*
+   * @pre username != null
+   * @pre password != null
+   */
+  public static void handleLoginClick(Controller controller, Hub hub, Client client, String username, String password) {
+    assert username != null;
+    assert password != null;
+    if (hub.isUser(username)) {
+      System.out.println("Logged in"); // Testing
+      controller.activeUser = hub.getUser(username, password);
+      if (controller.activeUser.isAdmin()) {
+        client.setView(controller.findBuilder(ViewType.HUB_ADMIN));
+      } else {
+        client.setView(controller.findBuilder(ViewType.HUB_BASIC));
+      }
+    } else {
+      client.alertUser(
+          AlertType.INFORMATION,
+          "Failure",
+          "Account does not exist",
+          "Username \"" + username + "\" is not in use. Please try a different one.");
+    }
+  }
+  
+  /*
+   * @pre username != null
+   * @pre password != null
+   */
+  public static void handleNewUser(Hub hub, Client client, String username, String password) {
+    assert username != null;
+    assert password != null;
+    if (acceptableInputs(client, username, password)) {
+      if (!hub.isUser(username)) {
+        hub.register(new UserAccount(hub, AccessLevel.BASIC, username, password));
+        client.alertUser(AlertType.INFORMATION, "Success", "Success", "User Account Created.");
+      } else {
+        client.alertUser(
+            AlertType.INFORMATION,
+            "Failure",
+            "Username Unavailable",
+            "Username \"" + username + "\" is already in use. Please try a different one.");
+      }
+    }
+  }
+  
+  /*
+   * @pre username != null
+   * @pre password != null
+   */
+  public static void handleNewAdmin(Hub hub, Client client, String username, String password) {
+    assert username != null;
+    assert password != null;
+    if (acceptableInputs(client, username, password)) {
+      if (!hub.isUser(username)) {
+        hub.register(new UserAccount(hub, AccessLevel.ADMIN, username, password));
+        client.alertUser(AlertType.INFORMATION, "Success", "Success", "Admin Account Created.");
+      } else {
+        client.alertUser(
+            AlertType.INFORMATION,
+            "Failure",
+            "Username Unavailable",
+            "Username \"" + username + "\" is already in use. Please try a different one.");
+      }
+    }
+  }
+  
+  /*
+   * @pre username != null
+   * @pre password != null
+   */
+  private static boolean acceptableInputs(Client client, String username, String password) {
+    assert username != null;
+    assert password != null;
+    if (username.toLowerCase().equals("username") || username.isEmpty()) {
+      client.alertUser(
+          AlertType.INFORMATION,
+          "Failure",
+          "Username Unaccepted",
+          "Username is not allowed to be \"username\" or left blank. Please try again");
+      return false;
+    } else if (password.toLowerCase() == "password" || password.isEmpty()) {
+      client.alertUser(
+          AlertType.INFORMATION,
+          "Failure",
+          "Password Unaccepted",
+          "Password is not allowed to be \"password\" or left blank. Please try again");
+      return false;
+    }
+    return true;
+  }
+}
