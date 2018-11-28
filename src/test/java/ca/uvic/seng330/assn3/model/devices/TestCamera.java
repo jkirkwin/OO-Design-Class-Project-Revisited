0e779e6bb4ab84565e14933393fe216394646275
Jamie Kirkwin <jkirkwin@gmail.com>
2018-11-27 23:29:57 -0800

    Re-add camera test file (empty)

--- src/test/java/ca/uvic/seng330/assn3/model/devices/TestCamera.java
+++ src/test/java/ca/uvic/seng330/assn3/model/devices/TestCamera.java
@@ -2,8 +2,13 @@ package ca.uvic.seng330.assn3.model.devices;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
+
 public class TestCamera {
-  //  @Test
+  
+  //TODO implement
+  
+  @Test
   public void testThrowsCameraFullException() {
     // TODO
     assertTrue(false);
